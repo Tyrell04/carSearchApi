b4ce92e5b4b6c84adbf7e1fe56de909d132cde70
marcschulz <marcleonschulz@gmail.com>
2023-04-10 16:19:05 +0200

    add UserResponse struct

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,8 +1,14 @@
 package models
 
-type LoginRequest struct {
+type UserModel struct {
 	Username string   `json:"username"`
 	Email    string   `json:"email"`
 	Password string   `json:"password"`
+	Roles    []string `json:"roles"`
+}
+
+type UserResponse struct {
+	Username string   `json:"username"`
+	Email    string   `json:"email"`
 	Roles    []string `json:"roles"`
 }
