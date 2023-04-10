1d3aa776754136277d4373561d9181ab9cedb6a1
marcschulz <marcleonschulz@gmail.com>
2023-04-10 16:17:06 +0200

    add jwt AccessTokenExpireDuration

--- config/model.go
+++ config/model.go
@@ -31,18 +31,19 @@ type Database struct {
 }
 
 type RedisConfig struct {
-	Host               string
-	Port               string
-	Password           string
-	Db                 string
-	DialTimeout        time.Duration
-	ReadTimeout        time.Duration
-	WriteTimeout       time.Duration
-	IdleCheckFrequency time.Duration
-	PoolSize           int
-	PoolTimeout        time.Duration
+	Host               string        `mapstructure:"host"`
+	Port               string        `mapstructure:"port"`
+	Password           string        `mapstructure:"password"`
+	Db                 string        `mapstructure:"db"`
+	DialTimeout        time.Duration `mapstructure:"dial_timeout"`
+	ReadTimeout        time.Duration `mapstructure:"read_timeout"`
+	WriteTimeout       time.Duration `mapstructure:"write_timeout"`
+	IdleCheckFrequency time.Duration `mapstructure:"idle_check_frequency"`
+	PoolSize           int           `mapstructure:"pool_size"`
+	PoolTimeout        time.Duration `mapstructure:"pool_timeout"`
 }
 
 type Jwt struct {
-	Secret string
+	Secret                    string        `mapstructure:"secret"`
+	AccessTokenExpireDuration time.Duration `mapstructure:"access_token_expire_duration"`
 }
