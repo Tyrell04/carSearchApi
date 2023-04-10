0202679c617e08959564eed8e0d908c8dcc02589
marcschulz <marcleonschulz@gmail.com>
2023-04-10 16:17:37 +0200

    added converter to UserResponse

--- entity/user.go
+++ entity/user.go
@@ -1,10 +1,26 @@
 package entity
 
+import "github.com/marcleonschulz/carSearchApi/pkg/models"
+
 type User struct {
-	Username  string     `gorm:"primaryKey;column:username;type:varchar(100)"`
+	Username  string     `gorm:"column:username;type:varchar(100)"`
 	Password  string     `gorm:"column:password;type:varchar(200)"`
+	Email     string     `gorm:"primaryKey;column:email;type:varchar(100)"`
 	IsActive  bool       `gorm:"column:is_active;type:boolean"`
-	UserRoles []UserRole `gorm:"ForeignKey:Username;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	UserRoles []UserRole `gorm:"ForeignKey:email;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+// ToResponse converts User to UserResponse
+func (user *User) ToResponse() models.UserResponse {
+	var roles []string
+	for _, userRole := range user.UserRoles {
+		roles = append(roles, userRole.Role)
+	}
+	return models.UserResponse{
+		Username: user.Username,
+		Email:    user.Email,
+		Roles:    roles,
+	}
 }
 
 func (User) TableName() string {
